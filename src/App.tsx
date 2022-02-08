b404f86fbb0fdcae20a0aeaaa251059009eebc17
Nettar-Jr <alabiabdulmumuni9@gmail.com>
2022-02-09 00:55:38 +1300

    feat: delete edit user

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,90 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
+
+import UsersList from "./UsersList"
+
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
   const [users, setUsers] = useState([]);
 
   async function handleSubmit(e) {
     e.preventDefault();
 
-    const result = await axios.post('https://fashsta-rest-test.herokuapp.com/users', {
-      email,
-      name,
-    });
+    const result = await axios.post(
+      "https://fashsta-rest-test.herokuapp.com/users",
+      {
+        email,
+        name,
+      }
+    );
 
     setMessage(result.data.message);
   }
 
   useEffect(() => {
-    axios.get('https://fashsta-rest-test.herokuapp.com/users', { email, name }).then((result) => {
-      setUsers(result.data);
-    });
+    axios
+      .get("https://fashsta-rest-test.herokuapp.com/users", { name, email })
+      .then((result) => {
+        setUsers([...result.data]);
+      });
 
     return () => {
-      setTimeout(() => setMessage(''), 1000);
+      setTimeout(() => setMessage(""), 1000);
     };
   }, [message, email, name]);
 
   return (
     <div className="asdadsfasd">
       <h1>Welcome to fashsta</h1>
-      <h4>{message}</h4>
+      <div style={{width: "70%", margin:"auto", height:"40px", textAlign: "center"}}>
+      <h4 style={{width: "70%", color:"green", margin:"auto"}}>{message}</h4>
+      </div>
 
       <form onSubmit={handleSubmit}>
         <input
+          id="userName"
           name="name"
           type="text"
+          placeholder="enter name"
           onChange={(event) => {
             setName(event.target.value);
           }}
         />
         <input
+          id="userEmail"
           name="email"
           type="email"
+          placeholder="enter email"
           onChange={(event) => {
             setEmail(event.target.value);
           }}
         />
-        <button type="submit">Signup</button>
+        <button id="update" type="submit">Signup</button>
       </form>
 
-      <h3>Users List</h3>
+      <h3 style={{width: '100%'}}>Users List</h3>
       {users.map((user, i) => (
         <div
+          id="users"
           key={user.email + i}
-          style={{
-            border: '1px solid white',
-            background: 'rgba(59, 100, 150, 0.5)',
-            width: 150,
-            height: 150,
-            margin: 10,
-          }}
         >
-          <div>{user.email}</div>
-          <div>{user.name}</div>
+          <div 
+          style={{
+            width:'100%',
+          }}>
+            <UsersList 
+              name={user.name}
+              email={user.email}
+              user={user}
+              refresh={handleSubmit}
+            />
+          </div>
         </div>
       ))}
     </div>
